c7194f63cdef68d516ad52e17467e41571c53353
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-23 20:46:20 +0200

    + remove deprecated log_mail-view

--- jobzwo/jobs/views.py
+++ jobzwo/jobs/views.py
@@ -17,18 +17,6 @@ def query(request, jobs):
 
 
 @apply_jobs
-def log_mail(request, jobs, job_id):
-    try:
-        job = jobs.get(id=job_id)
-    except Job.DoesNotExist:
-        raise Http404
-
-    log.info('Email-link clicked', job=job)
-
-    return HttpResponseRedirect('mailto:' + job.contact_email)
-
-
-@apply_jobs
 def log_external_url(request, jobs, job_id):
     try:
         job = jobs.get(id=job_id)
