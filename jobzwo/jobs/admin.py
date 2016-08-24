096b46fbc709037840faadd6bca9f9c82ed24209
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-24 11:17:12 +0200

    + add admin-action for state-transitions

--- jobzwo/jobs/admin.py
+++ jobzwo/jobs/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
 
+from .exceptions import JobStatusError, JobTransitionError
 from .models import Job
+from .querysets import JobStatusTransitionMixin
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', ]
+    actions = ['make_active', 'make_inactive', ]
+
+    readonly_fields = ('status',)
+
+    def make_active(self, request, queryset):
+        for job in queryset:
+            try:
+                job.transition(JobStatusTransitionMixin.STATUS_ACTIVE)
+            except (JobTransitionError, JobStatusError) as e:
+                self.message_user(
+                    request,
+                    'Transition failed for job-id "{}".'.format(e.job.id))
+
+    def make_inactive(self, request, queryset):
+        for job in queryset:
+            try:
+                job.transition(JobStatusTransitionMixin.STATUS_INACTIVE)
+            except (JobTransitionError, JobStatusError) as e:
+                self.message_user(
+                    request,
+                    'Transition failed for job-id "{}".'.format(e.job.id))
+
+
+admin.site.register(Job, JobAdmin)
+
