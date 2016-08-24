73aec04f40c00b1f67bfb875bb66934b1d3d4561
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-24 12:22:33 +0200

    + add impairment-filter

--- jobzwo/jobs/forms.py
+++ jobzwo/jobs/forms.py
@@ -12,8 +12,22 @@ class JobForm(forms.ModelForm):
 class SearchForm(forms.Form):
     q = forms.CharField(label='Search', required=False)
 
+    is_visual_impairment_accepted = forms.BooleanField(required=False)
+    is_hearing_impairment_accepted = forms.BooleanField(required=False)
+    is_motor_impairment_accepted = forms.BooleanField(required=False)
+
     def search(self, jobs):
         q = self.cleaned_data.get('q')
         if q:
             jobs = jobs.filter(title__icontains=q)
+
+        impairements_filter = {}
+        for name in [ 'visual', 'hearing', 'motor' ]:
+            impairment = 'is_{}_impairment_accepted'.format(name)
+            value = self.cleaned_data.get(impairment)
+            if value:
+                impairements_filter[impairment] = value
+        if impairements_filter:
+            jobs = jobs.filter(**impairements_filter)
+
         return jobs
