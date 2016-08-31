50a0ba5d269a0067b3993abc42c6b174c9c44308
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-31 17:21:33 +0200

    + improve tests for testing_sandbox.utils.rm

--- jobzwo/testing_sandbox/tests.py
+++ jobzwo/testing_sandbox/tests.py
@@ -6,8 +6,18 @@ from .utils import rm
 
 
 class RmTest(SimpleTestCase):
+    @mock.patch('testing_sandbox.utils.os.path')
     @mock.patch('testing_sandbox.utils.os')
-    def test_success(self, mock_os):
+    def test_missing_file(self, mock_os, mock_path):
+        mock_path.isfile.return_value = False
         rm('/not/existant')
 
-        mock_os.remove.assert_called_with("/not/existant")
+        self.assertFalse(mock_os.remove.called,
+                         "Failed to not remove the file if not present.")
+
+    @mock.patch('testing_sandbox.utils.os.path')
+    @mock.patch('testing_sandbox.utils.os')
+    def test_success(self, mock_os, mock_path):
+        mock_path.isfile.return_value = True
+        rm("/exists")
+        mock_os.remove.assert_called_with("/exists")
