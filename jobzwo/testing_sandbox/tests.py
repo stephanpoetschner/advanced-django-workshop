64dee787c907663fea999984f5ab2b367e4e5a0c
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-31 17:11:43 +0200

    + add mocked test for rm-call

--- jobzwo/testing_sandbox/tests.py
+++ jobzwo/testing_sandbox/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import mock
 
-# Create your tests here.
+from django.test import SimpleTestCase, TestCase
+
+from .utils import rm
+
+
+class RmTest(SimpleTestCase):
+    @mock.patch('testing_sandbox.utils.os')
+    def test_success(self, mock_os):
+        rm('/not/existant')
+
+        mock_os.remove.assert_called_with("/not/existant")
