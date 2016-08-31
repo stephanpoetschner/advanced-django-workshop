3cb88f42e9d6725a649660d8eb5d9b9b3f14165f
Stephan Poetschner <stephan.poetschner@gmail.com>
2016-08-31 17:02:30 +0200

    + add smoke test for /json/companies/

--- jobzwo/jobs/test_views.py
+++ jobzwo/jobs/test_views.py
@@ -5,3 +5,9 @@ class ListingTest(TestCase):
     def test_success(self):
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
+
+
+class JsonCompaniesTest(TestCase):
+    def test_success(self):
+        response = self.client.get('/json/companies/')
+        self.assertEqual(200, response.status_code)
